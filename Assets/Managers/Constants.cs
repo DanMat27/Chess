12a4b9889258b74dc73cb9cde8f575fa78eabcf0
DanMat27 <daniel07m@hotmail.es>
2022-06-30 19:38:23 +0200

    Added Constants script

---
+++ Assets/Managers/Constants.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Constants
+{
+    /* Constants */
+    public static string WHITE = "White";
+    public static string BLACK = "Black";
+    public static int EMPTY = 0;
+    public static int WHITE_PAWN = 1;
+    public static int BLACK_PAWN = 2;
+    public static int WHITE_HORSE = 3;
+    public static int BLACK_HORSE = 4;
+    public static int WHITE_BISHOP = 5;
+    public static int BLACK_BISHOP = 6;
+    public static int WHITE_TOWER = 7;
+    public static int BLACK_TOWER = 8;
+    public static int WHITE_QUEEN = 9;
+    public static int BLACK_QUEEN = 10;
+    public static int WHITE_KING = 11;
+    public static int BLACK_KING = 12;
+}
