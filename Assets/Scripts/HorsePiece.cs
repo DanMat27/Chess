272785dcb6891cbb84ff57faa283afc2c2ab107c
DanMat27 <daniel07m@hotmail.es>
2022-07-03 12:18:55 +0200

    Updated HorsePiece with normal possible moves finished

--- Assets/Scripts/HorsePiece.cs
+++ Assets/Scripts/HorsePiece.cs
@@ -43,6 +43,16 @@ public class HorsePiece : ChessPiece
         // Apply live movement to the dragged piece
         if (isDragging) {
             this.transform.position = mousePosition;
+
+            // Calculate the possible moves when dragged
+            if (!movesCalculated) {
+                moves = CurrentMovements();
+                movesCalculated = true;
+            }
+
+            // Show the possible moves of this piece on the board when dragging it
+            float curPos = (float)Math.Round(curX) + (float)Math.Round(curY)*(-1)*8;
+            if (GetShowMovesCallback() != null) GetShowMovesCallback()(moves, (int)curPos);
         }
 
         // When user drops the piece
@@ -53,27 +63,77 @@ public class HorsePiece : ChessPiece
             // If piece outside of board limits, apply position before movement
             if (outOfBounds(mousePosition.x, mousePosition.y)) {
                 comeBack();
+                cleanMoves();
                 return;
             }
 
-            // Send move to game manager and wait response
+            // Check if piece can move to the new position
             bool doMove = false;
-            if (GetMoveCallback() != null) {
-                float origin = (float)Math.Round(curX) + (float)Math.Round(curY)*(-1)*8;
-                float target = (float)Math.Round(mousePosition.x) + (float)Math.Round(mousePosition.y)*(-1)*8;
-                int curPiece = (this.tag == Constants.WHITE) ? Constants.WHITE_HORSE : Constants.BLACK_HORSE;
-                GetMoveCallback()((int)origin, (int)target, curPiece);
-            }
+            float origin = (float)Math.Round(curX) + (float)Math.Round(curY)*(-1)*8;
+            float target = (float)Math.Round(mousePosition.x) + (float)Math.Round(mousePosition.y)*(-1)*8;
+            if (moves.Contains((int)target)) doMove = true;
             
             // Apply movement or return to origin
-            if (doMove) ApplyAproxPiecePosition(mousePosition.x, mousePosition.y);
+            if (doMove) { 
+                // Send move to game manager to save it
+                if (GetMoveCallback() != null) {
+                    int curPiece = (this.tag == Constants.WHITE) ? Constants.WHITE_HORSE : Constants.BLACK_HORSE;
+                    GetMoveCallback()((int)origin, (int)target, curPiece);
+                }
+
+                ApplyAproxPiecePosition(mousePosition.x, mousePosition.y);
+                boardPos = (int)target;
+            }
             else comeBack();
+
+            // Reset moves
+            cleanMoves();
         }
     }
 
     // Returns valid moves of this piece in the current state
+    // Horse --> Can move in L shape any direction just 3 steps away
+    // Moves = [(x+1, y+2), (x-1, y+2), (x+2, y+1), (x+2, y-1), (x-2, y+1), (x-2, y-1), (x+1, y-2), (x-1, y-2)]
     public override List<int> CurrentMovements() 
     {
-        return moves;
+        bool userColor = GameState.Instance.GetUserColor(); // Color user is playing
+        bool turn = GameState.Instance.GetColorTurn(); // Current turn
+        bool curPieceColor = (this.tag == Constants.WHITE) ? true : false; // Color of the current piece
+        HashSet<int> curMoves = new HashSet<int>();
+
+        // If white turn and piece is black
+        if (!curPieceColor && turn) {
+            return new List<int>(curMoves);
+        }
+        // If black turn and piece is white
+        if (curPieceColor && !turn) {
+            return new List<int>(curMoves);
+        }
+
+        // Normal moves L up-left
+        int moveUpLeft = boardPos - (8 * 2 + 1);
+        if (moveUpLeft > minPos && Math.Abs(boardPos % 8 - moveUpLeft % 8) <= 1) curMoves.Add(moveUpLeft);
+        moveUpLeft = boardPos - (2 + 8);
+        if (moveUpLeft > minPos && Math.Abs(boardPos % 8 - moveUpLeft % 8) <= 2) curMoves.Add(moveUpLeft);
+
+        // Normal moves L up-right
+        int moveUpRight = boardPos - (8 * 2 - 1);
+        if (moveUpRight > minPos && Math.Abs(boardPos % 8 - moveUpRight % 8) <= 1) curMoves.Add(moveUpRight);
+        moveUpRight = boardPos - (8 - 2);
+        if (moveUpRight > minPos && Math.Abs(boardPos % 8 - moveUpRight % 8) <= 2) curMoves.Add(moveUpRight);
+
+        // Normal moves L down-left
+        int moveDownLeft = boardPos + (8 * 2 - 1);
+        if (moveDownLeft < maxPos && Math.Abs(boardPos % 8 - moveDownLeft % 8) <= 1) curMoves.Add(moveDownLeft);
+        moveDownLeft = boardPos + (8 - 2);
+        if (moveDownLeft < maxPos && Math.Abs(boardPos % 8 - moveDownLeft % 8) <= 2) curMoves.Add(moveDownLeft);
+        
+        // Normal moves L down-right
+        int moveDownRight = boardPos + (8 * 2 + 1);
+        if (moveDownRight < maxPos && Math.Abs(boardPos % 8 - moveDownRight % 8) <= 1) curMoves.Add(moveDownRight);
+        moveDownRight = boardPos + (2 + 8);
+        if (moveDownRight < maxPos && Math.Abs(boardPos % 8 - moveDownRight % 8) <= 2) curMoves.Add(moveDownRight);
+
+        return new List<int>(curMoves);
     }
 }
